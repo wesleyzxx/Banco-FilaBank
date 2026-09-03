c8c1dee5b03fafe560fb19d57fd81acab8050faa
wes <wesley.nunes@dcomp.ufs.br>
2026-09-03 13:14:59 -0300

    cliente.py

--- cliente.py
+++ cliente.py
@@ -1,13 +1,13 @@
 def nome(nome):
-    if nome.isalpha() or nome.isspace:
+    if nome.isalpha() and nome.isspace:
         return("Nome válido")
     else:
-        "Caracteres inválidos"
+        return("Caracteres inválidos")
 
 def docs(cpf, dataN):
     if cpf.isnumeric() and dataN.isnumeric():
         return("Documento válido")
     else:
-        "Caracteres inválidos"
+        return("Caracteres inválidos")
 print(nome(input('Qual é o seu nome: ')))
 print(docs(input('Qual é o seu cpf?: '), input('Em qual dia você nasceu?: ')))
